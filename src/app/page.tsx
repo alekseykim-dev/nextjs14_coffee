c6ad5675063ac9c27f129f8bb33a36d6dc7f26ab
Aleksey Kim <alex.romanovich.ak@gmail.com>
2024-04-30 05:43:37 +0900

    feat: google auth, login/logout, image parsing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import {faStar} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 export default function Home() {
   return (
-    <section className="max-w-lg mx-auto text-center mt-16">
+    <section className="max-w-lg mx-auto text-center mt-32">
       <div className="text-gray-600  ">
         {/* <p>
           <FontAwesomeIcon icon={faStar}/>
@@ -11,18 +11,18 @@ export default function Home() {
           <FontAwesomeIcon icon={faStar}/>
           <FontAwesomeIcon icon={faStar}/>
         </p> */}
-        <p className="mt-2">Loved by 100,000+ creators</p>
+        {/* <p className="mt-2">Loved by 100,000+ creators</p> */}
       </div>
-      <h1 className="text-6xl font-bold mt-4">
-        Fund your<br/>
-        creative work
+      <h1 className="text-4xl font-bold mt-4">
+        Fund me so that I could support BTC L2 solutions<br/>
+        {/* creative work */}
       </h1>
-      <h2 className="mt-4 mb-8">
-        Accept support for your work.<br/>
+      <h2 className="mt-4 mb-8 font-bold">
+        Stay bullish and HODL.<br/>
         It's easier than you think.
       </h2>
       <button className="bg-yellow-300 px-8 py-4 font-bold rounded-full">
-        Start my page
+        Start
       </button>
     </section>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,60 +1,62 @@
-'use client';
-import {faMugHot, faUser} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {Session} from "next-auth";
-import {signIn, signOut} from "next-auth/react";
-import Image from "next/image";
-import Link from "next/link";
-import {parseFullName} from "parse-full-name";
+'use client'
+import { faBitcoinSign, faMugHot, faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Session } from 'next-auth'
+import { signIn, signOut } from 'next-auth/react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { parseFullName } from 'parse-full-name'
 
-export default function Header({session}:{session:Session|null}) {
-  const name = session?.user?.name || '';
-  const {first:firstName} = parseFullName(name);
+export default function Header({ session }: { session: Session | null }) {
+	const name = session?.user?.name || ''
+	const { first: firstName } = parseFullName(name)
 
-  return (
-    <>
-      <header className="bg-white">
-        <div className="flex justify-between max-w-2xl mx-auto px-4 py-4">
-          <Link href={'/'} className="inline-flex gap-1 items-center">
-            <FontAwesomeIcon className="h-8" icon={faMugHot}/>
-            <span className="mt-2">Buy me a coffee</span>
-          </Link>
-          <nav className="mt-2 flex gap-6 items-center">
-            <Link href="/about">About</Link>
-            <Link href="/about">FAQ</Link>
-            <Link href="/about">Contact</Link>
-            <div className="flex gap-4">
-              {session && (
-                <div className="">
-                  <Link
-                    href={"/profile"}
-                    className="flex items-center gap-2 bg-yellow-300 rounded-full p-1 pr-4">
-                    <Image src={session.user?.image as string}
+	return (
+		<>
+			<header className='bg-white'>
+				<div className='flex justify-between max-w-2xl mx-auto px-4 py-4'>
+					<Link href={'/'} className='inline-flex gap-1 items-center'>
+						<FontAwesomeIcon className='h-8' icon={faBitcoinSign} />
+						<span className='mt-2'>Send crypto</span>
+					</Link>
+					<nav className='mt-2 flex gap-6 items-center'>
+						<Link href='/about'>About</Link>
+						<Link href='/about'>FAQ</Link>
+						<Link href='/about'>Contact</Link>
+						<div className='flex gap-4'>
+							{session && (
+								<div className='flex items-center gap-2  rounded-full p-1 pr-4'>
+									 <Image src={session.user?.image as string}
                            alt="avatar"
                            width="36" height="36"
                            className="rounded-full"
                     />
-                    {firstName}
-                  </Link>
-                </div>
-              )}
-              {!session && (
-                <>
-                  <button
-                    onClick={() => signIn('google')}
-                    className="border-2 rounded-full px-4 py-2 ml-4"
-                  >
-                    Login
-                  </button>
-                  <button className="bg-yellow-300 rounded-full px-4 py-2">
-                    Sign up
-                  </button>
-                </>
-              )}
-            </div>
-          </nav>
-        </div>
-      </header>
-    </>
-  );
+									{firstName}
+									<button
+										onClick={() => signOut()}
+										className='rounded-full px-4 py-2 ml-4 bg-yellow-300'
+									>
+										Logout
+									</button>
+								</div>
+							)}
+							{!session && (
+								<div className='flex items-center gap-2  rounded-full p-1 pr-4'>
+									<button
+										onClick={() => signIn('google')}
+										className='border-2 rounded-full px-4 py-2 ml-4'
+									>
+										Login
+									</button>
+									<button className='bg-yellow-300 rounded-full px-4 py-2'>
+										Sign up
+									</button>
+								</div>
+							)}
+						</div>
+					</nav>
+				</div>
+			</header>
+		</>
+	)
 }
