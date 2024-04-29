4498eceeee51d0d138416c5fdfe020e0ad3338c6
Aleksey Kim <alex.romanovich.ak@gmail.com>
2024-04-30 05:05:29 +0900

    feat: project init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Header from '@/components/Header';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <body className={inter.className}>
+        <Header session={null}/>
+        {children}
+        </body>
+    </html> 
   );
 }
+
